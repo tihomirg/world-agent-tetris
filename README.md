e29a2ef9b829bf2d428144707cf509ae015f5594
tihomirg <tihomir.gvero@gmail.com>
2026-08-07 16:24:39 +0200

    Improving comments in the code and giving simple details in the readme.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# world-agent-tetris
+## Purpose
+The purpose is to build an agent trained in a custom world model. 
+
+We chose Tetris game as the basis for building the custom world model.
+
+## Key Steps
+1. Generate data from sessions of Tetris games played in Gymnasium.
+2. Use the data to create a world model that will simulate Tetris game.
+3. Use the world model to train Tetris agent.
--- gym-begginer.py
+++ gym-begginer.py
@@ -1,7 +1,9 @@
 import gymnasium as gym
 
+# make environment and render every step for human to see it
 env = gym.make("CartPole-v1", render_mode="human")
 
+# first observation
 observation, info = env.reset()
 
 print(f"Starting observation {observation}")
@@ -9,15 +11,24 @@ print(f"Starting observation {observation}")
 episode_over = False
 total_reward = 0
 
+# loop and play in the environment
 while not episode_over:
-    # chose an action
+
+    # chose a random action
+    # action space contains all the possible actions of the agent in the environment
     action = env.action_space.sample()
 
-    # take the action and see the new observation
+    # apply the action to the environment and current state
     observation, reward, terminated, truncated, info = env.step(action)
 
-    total_reward += 1
+    # after taking the action the reward can be positive if we are getting closer or negative if we are getting further from the goal
+    total_reward += reward
+
+    # terminated means that we reached the losing state (we lost)
+    # truncated means we reached the set time limit 
     episode_over = terminated or truncated
 
 print(f"Episode finished! Total reward: {total_reward}")
+
+#at the end we need to clos the environment
 env.close()
